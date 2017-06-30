94a6c7da331b626849d670c341e675bb0a488e47
Michel LIBERADO <michel.liberado@guerbet-group.com>
2017-06-30 16:26:42 +0200

    Updating a DICOM blob directly in the database works now. Now the goal is to find which tags should be anonymised.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # parseDicomBlob
 little project to understand dicom blobs in dcm4chee v2 database
+
+Tags to anonymise and retrieved from DCM4CHEE and found in the configuration files:
+
+https://dcm4che.atlassian.net/wiki/display/ee2/DICOM+Attribute+Filter
+https://groups.google.com/forum/#!topic/dcm4che/zkT4O7ZOY6c
+
+
+    <dcm4chee-attribute-filter>
+        <patient>
+            <attr tag="00100010" case-sensitive="false"/> <!-- Patient's Name -->
+            <attr tag="00100030"/> <!-- Patient's Birth Date -->
+            <attr tag="00101005"/> <!-- Patient's Birth Date -->
+            <attr tag="00100032"/> <!-- Patient's Birth Time -->
+            <attr tag="00101040"/> <!-- Patient's Address -->
+            <attr tag="00101001"/> <!-- Other Patient Names -->
+            <attr tag="00101060"/> <!-- Patient Mothers Maiden Names -->
+            <attr tag="00102154" field="patientCustomAttribute1"/> <!-- Patient Phone number -->
+          </patient>
+          <study>
+            <attr tag="00080020"/> <!-- Study Date -->
+            <attr tag="00080030"/> <!-- Study Time -->
+            <attr tag="00080090" case-sensitive="false"/> <!-- Referring Physician Name -->
+            <attr tag="00081030" case-sensitive="false"/> <!-- Study Description -->
+            <attr tag="00081048" field="studyCustomAttribute1"/> <!-- Physician(s) of Record -->
+            <attr tag="00081060"/> <!-- Name of Physician(s) Reading Study -->
+          </study>
+          <series>
+            <attr tag="00080060"/> <!-- Modality -->
+            <attr tag="00080068"/> <!-- Presentation Intent Type -->
+            <attr tag="00081050" case-sensitive="false"/> <!-- Performing Physicians' Name -->
+            <attr tag="00081070"/> <!-- Operators' Name -->
+          </series>
+          <instance>
+                 <attr seq="0040A073" tag="0040A075" case-sensitive="false"/> <!-- Verifying Observer Name -->
+                 <attr tag="00700084"/> <!-- Content Creator s Name -->
+         </instance>
+    </dcm4chee-attribute-filter>
--- src/main/java/com/liberado/util/Metadata.java
+++ src/main/java/com/liberado/util/Metadata.java
@@ -56,8 +56,62 @@ public class Metadata {
 
         //testPatientDAO();
         //testMwl_itemDAO();
-        testUpdatePatientInDatabase();
+        //testUpdatePatientInDatabase();
+
+        // Patient Tags
+        //Tags.PatientName;
+        //Tags.PatientBirthDate;
+        //Tags.PatientBirthName;
+        //Tags.PatientBirthTime;
+        //Tags.PatientAddress;
+        //Tags.OtherPatientNames;
+        //Tags.PatientMotherBirthName;
+        //Tags.PatientPhoneNumbers;
+
+        // Study Tags
+        //Tags.StudyDate;
+        //Tags.StudyTime;
+        //Tags.ReferringPhysicianName;
+        //Tags.NameOfPhysicianReadingStudy
+
+        // Series Tags
+        //Tags.OperatorName;
+
+        //
     }
+/*
+
+        <dcm4chee-attribute-filter>
+        <patient>
+            <attr tag="00100010" case-sensitive="false"/> <!-- Patient's Name -->
+            <attr tag="00100030"/> <!-- Patient's Birth Date -->
+            <attr tag="00101005"/> <!-- Patient's Birth Name -->
+            <attr tag="00100032"/> <!-- Patient's Birth Time -->
+            <attr tag="00101040"/> <!-- Patient's Address -->
+            <attr tag="00101001"/> <!-- Other Patient Names -->
+            <attr tag="00101060"/> <!-- Patient Mothers Maiden Names -->
+            <attr tag="00102154" field="patientCustomAttribute1"/> <!-- Patient Phone number -->
+          </patient>
+          <study>
+            <attr tag="00080020"/> <!-- Study Date -->
+            <attr tag="00080030"/> <!-- Study Time -->
+            <attr tag="00080090" case-sensitive="false"/> <!-- Referring Physician Name -->
+            <attr tag="00081030" case-sensitive="false"/> <!-- Study Description -->
+            <attr tag="00081048" field="studyCustomAttribute1"/> <!-- Physician(s) of Record -->
+            <attr tag="00081060"/> <!-- Name of Physician(s) Reading Study -->
+          </study>
+          <series>
+            <attr tag="00080060"/> <!-- Modality -->
+            <attr tag="00080068"/> <!-- Presentation Intent Type -->
+            <attr tag="00081050" case-sensitive="false"/> <!-- Performing Physicians' Name -->
+            <attr tag="00081070"/> <!-- Operators' Name -->
+          </series>
+          <instance>
+                 <attr seq="0040A073" tag="0040A075" case-sensitive="false"/> <!-- Verifying Observer Name -->
+                 <attr tag="00700084"/> <!-- Content Creator s Name -->
+         </instance>
+    </dcm4chee-attribute-filter>*/
+
 
     /**
      * @param base64DICOM parameter fetched from the database, ie. select encode(pat_attrs, 'base64') from patient where pat_id = 'A10504795685'
@@ -185,7 +239,7 @@ public class Metadata {
         Dataset ds = fromByteArray(aPatient.getPat_attrs());
         if (ds != null) {
             PersonName personName = ds.getPersonName(Tags.PatientName);             // Extract the PersonName object
-            aPatient.setPat_name(personName.get(PersonName.FAMILY) + "-TOTO");      // Modify in the column in 'clear'
+            aPatient.setPat_name(personName.get(PersonName.FAMILY) + "-DOE");      // Modify in the column in 'clear'
             personName.set(PersonName.FAMILY, aPatient.getPat_name());              // Modify in the PersonName object
             ds.putPN(Tags.PatientName, personName);                                 // Modify in the DICOM dataset
             aPatient.setPat_attrs(toByteArray(ds));                                 // Put back the DICOM attributes
